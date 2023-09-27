a5dbd6fa1a7ebbdda439a1e5bede3414631de710
Matthew Gomes <matthew.h.gomes@gmail.com>
2023-09-27 10:21:00 +0200

    connection improvements

--- sailbot_ws/src/sailbot/src/network_comms.py
+++ sailbot_ws/src/sailbot/src/network_comms.py
@@ -7,12 +7,15 @@ import rclpy
 from rclpy.node import Node
 import threading
 
+
 class Wind:
     speed = 0
     direction = 0
+
     def __init__(self, speed, direction):
         self.speed = speed
         self.direction = direction
+
 
 class BoatState:
     latitude = 0
@@ -36,6 +39,7 @@ class BoatState:
     pitch = 0
     roll = 0
 
+
 class NetworkComms(Node):
 
     current_boat_state = BoatState()
@@ -47,14 +51,17 @@ class NetworkComms(Node):
         super().__init__('control_system')
         self.server_socket.bind(("0.0.0.0", 1111))  # Bind to a specific address and port
         self.server_socket.listen(1)  # Listen for incoming connections
-        print("Server is listening for incoming connections...")
+        self.get_logger().info("Server is listening for incoming connections...")
+        self.create_timer(0.25, self.update_clients)
+        client_registration_thread = threading.Thread(target=self.register_clients_loop, daemon=True)
+        client_registration_thread.start()
 
     def register_clients(self):
         read_sockets, write_sockets, error_sockets = select.select([self.server_socket] , [], [])
         for sock in read_sockets:
             client_socket, client_address = self.server_socket.accept()
             self.client_addresses.add(client_address[0])
-            print(f"Accepted connection from {client_address}")
+            self.get_logger().info(f"Accepted connection from {client_address}")
             # Data to be sent to the client
             data_to_send = {"registered"}
             #  Serialize and send the data
@@ -63,38 +70,34 @@ class NetworkComms(Node):
             # Close the client socket
             client_socket.close()
 
-    def update_clients(self):
-        for address in self.client_addresses:
-            self.get_logger().info(address)
-            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def update_single_client(self, address, data):
+        self.get_logger().info(address)
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.setblocking(0)
+        client_socket.settimeout(0.1)
+        try:
             client_socket.connect((address, 1111))
-            data_bytes = pickle.dumps(self.current_boat_state)
-            client_socket.send(data_bytes)
+            client_socket.send(data)
             client_socket.close()
-        
+        except:
+             self.get_logger().info(f"Lost connection to client: {address}")
 
-    def register_and_update_clients(self):
-        self.register_clients()
-        self.update_clients()
+    def update_clients(self):
+        self.get_logger().info(f"Num clients: {len(self.client_addresses)}")
+        data_bytes = pickle.dumps(self.current_boat_state)
+        for address in self.client_addresses:
+            update_client_thread = threading.Thread(target=self.update_single_client, args=(address, data_bytes))
+            update_client_thread.start()
+        
+    def register_clients_loop(self):
+        while True:
+            self.register_clients()
 
 def main(args=None):
     rclpy.init(args=args)
-
     network_comms = NetworkComms()
-    
-    thread = threading.Thread(target=rclpy.spin, args=(network_comms, ), daemon=True)
-    thread.start()
-        
-    rate = network_comms.create_rate(4)
-    try:
-        while rclpy.ok():
-            network_comms.register_and_update_clients()
-            rate.sleep()
-    except KeyboardInterrupt:
-        pass
-
+    rclpy.spin(network_comms)
     rclpy.shutdown()
-    thread.join()
 
 if __name__ == "__main__":
     main()
