c8a6a094a5a86f51ca62d1918b9bffbcf90fa359
Matthew Gomes <matthew.h.gomes@gmail.com>
2023-10-16 00:44:39 -0400

    rudimentary control messages

--- sailbot_ws/src/sailbot/src/network_comms.py
+++ sailbot_ws/src/sailbot/src/network_comms.py
@@ -35,7 +35,7 @@ class NetworkComms(Node):
 
 
     def update_clients(self):
-        self.get_logger().info(f"Num clients: {len(self.client_sockets)}")
+        #self.get_logger().info(f"Num clients: {len(self.client_sockets)}")
         data_bytes = pickle.dumps(self.current_boat_state)
 
         for host in self.client_sockets.keys():
@@ -43,6 +43,28 @@ class NetworkComms(Node):
                 self.client_sockets[host].send(data_bytes)
             except:
                 self.get_logger().info(f"Lost connection to client: {str(host)}")
+    
+    def receive_commands(self):
+        while True:
+            rlist, _, _ = select.select(self.client_sockets.values(), [], [], 0.2)
+            for sock in rlist:
+                try:
+                    received_bytes = sock.recv(1024)
+                    received_data = pickle.loads(received_bytes)
+                except:
+                    continue #connection lost, should be re-established by main thread
+                match received_data:
+                    case ControlCommand():
+                        match received_data.control_type:
+                            case ControlType.TRIM_TAB:
+                                self.get_logger().info("Received trimtab control command: "+str(received_data.control_value))
+                            case ControlType.RUDDER:
+                                self.get_logger().info("Received rudder control command: "+str(received_data.control_value))
+                            case _:
+                                self.get_logger().info("Unknown control command received")
+                    case _:
+                        self.get_logger().info("Unknown command received")
+                pass
         
     def register_clients_loop(self):
         while True:
@@ -51,6 +73,8 @@ class NetworkComms(Node):
 def main(args=None):
     rclpy.init(args=args)
     network_comms = NetworkComms()
+    receive_thread = threading.Thread(target=network_comms.receive_commands, daemon=True)
+    receive_thread.start()
     rclpy.spin(network_comms)
     rclpy.shutdown()
 
