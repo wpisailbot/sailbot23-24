95195791828e2bd8311f9d26180879527643daa6
matthew.h.gomes@gmail.com <matthew.h.gomes@gmail.com>
2024-05-30 03:38:55 -0400

    fix startup script, new launchfile

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 sudo mkdir -p /opt/sailbot
 sudo cp -r startup/* /opt/sailbot/
+chmod a+x /opt/sailbot/run.sh
 sudo cp startup/systemd/ros2_control_server.service /etc/systemd/system/ros2_control_server.service
 sudo systemctl daemon-reload
 sudo systemctl enable ros2_control_server.service
